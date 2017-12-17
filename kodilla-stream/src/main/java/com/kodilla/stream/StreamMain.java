d6f9c6f9663e41d2c0970f4f767ec3ae9c661a4a
paderewska <iza.paderewska@gmail.com>
2017-12-17 19:46:48 +0100

    7.1. Zadanie: Upiekszacz tekstow

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,34 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import static jdk.nashorn.internal.objects.NativeString.substr;
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("Merry Christmas", (text) -> text + "!" );
+        poemBeautifier.beautify("Merry Christmas", (text) -> text + " <3" );
+        poemBeautifier.beautify("Merry Christmas", (text) -> "Pawel " + text + "!" );
+        poemBeautifier.beautify("Merry Christmas", (text) -> toUpperCase(text));
+        poemBeautifier.beautify("Merry Christmas", (text) -> text.substring(0,6) + "and happy " + text.substring(6,15));
+
+        System.out.println("\nCalculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB);
+    }
+}
