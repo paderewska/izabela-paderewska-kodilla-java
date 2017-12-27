09ced84b21016643e7a11b1047b3886ec41c488e
paderewska <iza.paderewska@gmail.com>
2017-12-27 16:43:34 +0100

    7.5 poprawione mapowanie

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -8,6 +8,7 @@ public interface ArrayOperations {
     static double getAverage(int[] numbers) {
 
         IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
                 .forEach(System.out::println);
 
         OptionalDouble numbersAverage = IntStream.range(0, numbers.length)
