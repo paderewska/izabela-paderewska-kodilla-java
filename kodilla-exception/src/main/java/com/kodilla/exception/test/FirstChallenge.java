fd276ce94807284bc9bb571694c495a1433a44bb
paderewska <iza.paderewska@gmail.com>
2018-01-02 14:36:09 +0100

    8.2 Poprawki

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,26 +3,23 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
-            return a / b;
-
-        } catch (ArithmeticException e) {
-            System.out.println("Nie dziel przez zero! " + e);
-            throw e;
-
-        } finally {
-            System.out.println("Koniec obliczen");
+        if(b == 0){
+            throw new ArithmeticException();
         }
+        return a / b;
     }
 
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
-        System.out.println(result);
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Nie dzieli si przez ZERO! "+e);
+        } finally {
+            System.out.println("Koniec obliczen");
+        }
     }
 }
