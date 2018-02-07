ece345fd14e95525bf2c8af84f47956ce69df025
paderewska <iza.paderewska@gmail.com>
2018-02-07 22:50:00 +0100

    Dodatkowe zadanie

--- kodilla-stream/src/main/java/com/kodilla/stream/niefunkcyjnie/Main.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/niefunkcyjnie/Main.java
@@ -1,7 +1,10 @@
 package com.kodilla.stream.niefunkcyjnie;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -18,6 +21,13 @@ public class Main {
         return counterek;
     }
 
+    public static int getCountEmptyStringUsingJava8(List<String> theList) {
+
+        return (int) theList.stream()
+                .filter(n -> n.isEmpty())
+                .count();
+    }
+
     public static int getCountLength3UsingJava7(List<String> theList) {
 
         int counterek = 0;
@@ -29,6 +39,13 @@ public class Main {
             }
         }
         return counterek;
+    }
+
+    public static int getCountLength3UsingJava8(List<String> theList) {
+
+    return (int) theList.stream()
+            .filter(n -> n.length() == 3)
+            .count();
     }
 
     public static List<String> deleteEmptyStringsUsingJava7(List<String> strings) {
@@ -43,6 +60,13 @@ public class Main {
         return theList;
     }
 
+    public static List<String> deleteEmptyStringsUsingJava8(List<String> strings) {
+
+    return strings.stream()
+            .filter(n -> !n.isEmpty())
+            .collect(Collectors.toList());
+    }
+
 
     public static String getMergedStringUsingJava7(List<String> strings, String s) {
         List<String> theList = new ArrayList<>();
@@ -52,14 +76,42 @@ public class Main {
                 theList.add(strings.get(i));
             }
         }
-        return "cos";
+        String result = String.join(s, theList);
+        return result;
+    }
+
+    public static String getMergedStringUsingJava8(List<String> strings, String s) {
+
+        return strings.stream()
+                .filter(n -> !n.isEmpty())
+                .collect(Collectors.joining(s));
     }
 
     public static List<Integer> getSquares(List<Integer> numbers) {
-        return new ArrayList<>();
+        List<Integer> theList = new ArrayList<>();
+
+        for(Integer number : numbers) {
+            Integer squareNumber = number * number;
+            if(!theList.contains(squareNumber)) {
+                theList.add(squareNumber);
+            }
+        }
+        return theList;
+    }
+
+    public static List<Integer> getSquaresJava8(List<Integer> numbers) {
+
+        return numbers.stream()
+                .map(n -> n = n*n)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public static Integer getMax(List<Integer> numbers) {
+        return 0;
+    }
+
+    public static Integer getMaxJava8(List<Integer> numbers) {
         return 0;
     }
 
@@ -67,11 +119,24 @@ public class Main {
         return 0;
     }
 
+    public static Integer getMinJava8(List<Integer> numbers) {
+        return 0;
+    }
+
     public static Integer getSum(List<Integer> numbers) {
         return 0;
     }
 
+    public static Integer getSumJava8(List<Integer> numbers) {
+
+        return 0;
+    }
+
     public static Integer getAverage(List<Integer> numbers) {
+        return 0;
+    }
+
+    public static Integer getAverageJava8(List<Integer> numbers) {
         return 0;
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/niefunkcyjnie/TestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/niefunkcyjnie/TestSuite.java
@@ -19,11 +19,31 @@ public class TestSuite {
     }
 
     @Test
+    public void testCountEmptyStringUsingJava8() {
+        //given
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+        //when
+        long count = Main.getCountEmptyStringUsingJava8(strings);
+        //then
+        Assert.assertEquals(2, count);
+    }
+
+    @Test
     public void testCountLength3UsingJava7() {
         //given
         List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
         //when
         long count = Main.getCountLength3UsingJava7(strings);
+        //then
+        Assert.assertEquals(3, count);
+    }
+
+    @Test
+    public void testCountLength3UsingJava8() {
+        //given
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+        //when
+        long count = Main.getCountLength3UsingJava8(strings);
         //then
         Assert.assertEquals(3, count);
     }
@@ -41,12 +61,35 @@ public class TestSuite {
     }
 
     @Test
+    public void testDeleteEmptyStringsUsingJava8() {
+        //given
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+        List<String> expected = Arrays.asList("abc", "bc", "efg", "abcd", "jkl");
+        //when
+        List<String> result = Main.deleteEmptyStringsUsingJava8(strings);
+        //then
+        Assert.assertEquals(5, result.size());
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
     public void testMergedStringUsingJava7() {
         //given
         List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
         String expected = "abc,bc,efg,abcd,jkl";
         //when
         String result = Main.getMergedStringUsingJava7(strings, ",");
+        //then
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMergedStringUsingJava8() {
+        //given
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+        String expected = "abc,bc,efg,abcd,jkl";
+        //when
+        String result = Main.getMergedStringUsingJava8(strings, ",");
         //then
         Assert.assertEquals(expected, result);
     }
@@ -64,12 +107,35 @@ public class TestSuite {
     }
 
     @Test
+    public void testSquaresJava8() {
+        //given
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        List<Integer> expected = Arrays.asList(9, 4, 49, 25);
+        //when
+        List<Integer> result = Main.getSquaresJava8(numbers);
+        //then
+        Assert.assertEquals(4, result.size());
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
     public void testMax() {
         //given
         List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
         Integer expected = 7;
         //when
         Integer result = Main.getMax(numbers);
+        //then
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMaxJava8() {
+        //given
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        Integer expected = 7;
+        //when
+        Integer result = Main.getMaxJava8(numbers);
         //then
         Assert.assertEquals(expected, result);
     }
@@ -86,6 +152,17 @@ public class TestSuite {
     }
 
     @Test
+    public void testMinJava8() {
+        //given
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        Integer expected = 2;
+        //when
+        Integer result = Main.getMinJava8(numbers);
+        //then
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
     public void testSum() {
         //given
         List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
@@ -97,12 +174,34 @@ public class TestSuite {
     }
 
     @Test
+    public void testSumJava8() {
+        //given
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        Integer expected = 25;
+        //when
+        Integer result = Main.getSumJava8(numbers);
+        //then
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
     public void testAverage() {
         //given
         List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
         Integer expected = 3;
         //when
         Integer result = Main.getAverage(numbers);
+        //then
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testAverageJava8() {
+        //given
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        Integer expected = 3;
+        //when
+        Integer result = Main.getAverageJava8(numbers);
         //then
         Assert.assertEquals(expected, result);
     }
