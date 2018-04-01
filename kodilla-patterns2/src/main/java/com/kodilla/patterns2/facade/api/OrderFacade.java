b60376d4c1ca5c77a3eb1cf3ebbca1dc1ac6639c
paderewska <iza.paderewska@gmail.com>
2018-04-01 13:55:33 +0200

    20.3 Programowanie aspektowe w Spring Boot

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -15,7 +15,11 @@ public class OrderFacade {
     private ShopService shopService;
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
 
-    public void processOrder(final OrderDto order, final Long userId) throws OrderProcessingException {
+    @Autowired
+    private OrderWatcher orderWatcher;
+
+
+    public void processOrder(OrderDto order, Long userId) throws OrderProcessingException {
         boolean wasError = false;
         long orderId = shopService.openOrder(userId);
         LOGGER.info("Registering new order, ID: " + orderId);
